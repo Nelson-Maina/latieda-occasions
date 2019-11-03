a35b84a282bb1bb661ee157c39321d88ca71fd77
FELIX OMUOK <dijiflex@gmail.com>
2019-11-03 17:57:57 +0300

    Added the added the session check in the admin page and changed the admin header

--- admin.php
+++ admin.php
@@ -1,3 +1,11 @@
+<?php
+require 'includes/init.php';
+
+if ($_SESSION['user_type'] !== "admin") {
+    redirect("index.html");
+}
+?>
+
 <!--
 Author: W3layouts
 Author URL: http://w3layouts.com
@@ -9,21 +17,20 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <html lang="zxx">
 
 <head>
-    <title>Mechanized Industrial Category Bootstrap Responsive Web Template | Single :: W3layouts</title>
+    <title>Latida Admin</title>
     <!-- Meta tag Keywords -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8" />
     <meta name="keywords"
         content="Mechanized Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
     <script>
-        addEventListener("load", function () {
+        addEventListener("load", function() {
             setTimeout(hideURLbar, 0);
         }, false);
 
         function hideURLbar() {
             window.scrollTo(0, 1);
         }
-
     </script>
     <!-- //Meta tag Keywords -->
     <!-- Custom-Files -->
@@ -35,7 +42,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <!-- Style-CSS -->
     <!-- font-awesome-icons -->
     <!-- <link href="css/font-awesome.css" rel="stylesheet"> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+        integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
     <!-- //font-awesome-icons -->
     <!-- /Fonts -->
     <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,600,700"
@@ -93,8 +101,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             </div>
             <div class="row">
                 <div class="col">
-                    <button class="btn btn-outline-success" data-toggle="modal"
-                    data-target="#RegNewMemberModal"> <span><i class="fas fa-plus-circle"></i></span> Add Past Event</button>
+                    <button class="btn btn-outline-success" data-toggle="modal" data-target="#RegNewMemberModal">
+                        <span><i class="fas fa-plus-circle"></i></span> Add Past Event</button>
                 </div>
             </div>
 
@@ -353,61 +361,62 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <!-- //footer -->
 
     <!--member new post Modal -->
-<div class="modal fade" id="RegNewMemberModal" tabindex="-1" role="dialog" aria-labelledby="RegNewMemberModal"
-aria-hidden="true">
-<div class="modal-dialog modal-dialog-scrollable" role="document">
-    <div class="modal-content">
-        <div class="modal-header" style="background-color:  #f6dffa;">
-            <h5 class="modal-title" id="exampleModalScrollableTitle">Enter Past Event Details</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <script src="ckeditor/ckeditor.js"></script>
-       
+    <div class="modal fade" id="RegNewMemberModal" tabindex="-1" role="dialog" aria-labelledby="RegNewMemberModal"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color:  #f6dffa;">
+                    <h5 class="modal-title" id="exampleModalScrollableTitle">Enter Past Event Details</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <script src="ckeditor/ckeditor.js"></script>
 
-        <div class="modal-body py-0 px-1">
-            <!-- //form to create a new post -->
-            <form id="usernewpost" action="includes/article.inc.php" enctype="multipart/form-data" method="post">
-                
-                <div class="form-group ">
 
-                    <input type="text" class="form-control form-control-sm" id="lastName"
-                        aria-describedby="emailHelp" placeholder="Enter Event Title" name="post-title" required>
+                <div class="modal-body py-0 px-1">
+                    <!-- //form to create a new post -->
+                    <form id="usernewpost" action="includes/article.inc.php" enctype="multipart/form-data"
+                        method="post">
+
+                        <div class="form-group ">
+
+                            <input type="text" class="form-control form-control-sm" id="lastName"
+                                aria-describedby="emailHelp" placeholder="Enter Event Title" name="post-title" required>
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlFile1">Upload Image Here</label>
+                            <input type="file" class="form-control-file form-control-sm" name="image"
+                                id="exampleFormControlFile1">
+                        </div>
+
+
+                        <textarea name="content" placeholder="Enter Event Description"></textarea>
+                        <script>
+                            CKEDITOR.replace('content');
+                        </script>
+                    </form>
 
                 </div>
-
-                <div class="form-group">
-                    <label for="exampleFormControlFile1">Upload Image Here</label>
-                    <input type="file" class="form-control-file form-control-sm" name="image"
-                        id="exampleFormControlFile1">
+                <div class="modal-footer" style="background-color:  #f6dffa;">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancle</button>
+                    <button type="submit" class="btn btn-success" form="usernewpost" name="post_article">Post</button>
                 </div>
-
-
-                <textarea name="content" placeholder="Enter Event Description"></textarea>
-                <script>
-                    CKEDITOR.replace('content');
-                </script>
-            </form>
-
-        </div>
-        <div class="modal-footer" style="background-color:  #f6dffa;">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancle</button>
-            <button type="submit" class="btn btn-success" form="usernewpost" name="post_article">Post</button>
+            </div>
         </div>
     </div>
-</div>
-</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
 
 </body>
